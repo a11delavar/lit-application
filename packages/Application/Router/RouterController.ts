c9978c1d8cf9a5e04afd3c5057cf8af33d4acc5f
arshia11d <arshia11d@outlook.com>
2022-11-16 01:44:45 +0100

    refactor(Application): No private property access in router

--- packages/Application/Router/RouterController.ts
+++ packages/Application/Router/RouterController.ts
@@ -1,18 +1,26 @@
 import { Router as RouterControllerBase } from '@lit-labs/router'
+import { ReactiveControllerHost } from '@a11d/lit'
 import { PageComponent } from '../index.js'
 import { Router } from './Router.js'
 
 export class RouterController extends RouterControllerBase {
+	protected readonly host: ReactiveControllerHost & HTMLElement
+
 	override hostConnected() {
 		this.importDecoratorRoutesIfAvailable()
 		super.hostConnected()
+	}
+
+	constructor(...args: ConstructorParameters<typeof RouterControllerBase>) {
+		super(...args)
+		this.host = args[0]
 	}
 
 	private readonly pagesByRoute = new Map<string, PageComponent>()
 
 	private importDecoratorRoutesIfAvailable() {
 		const decoratorRoutes = [...Router.container]
-			.filter(([, { routerHostConstructor }]) => this['_host'].constructor === routerHostConstructor || this['_host'].constructor.prototype instanceof routerHostConstructor)
+			.filter(([, { routerHostConstructor }]) => this.host.constructor === routerHostConstructor || this.host.constructor.prototype instanceof routerHostConstructor)
 			.map(([route, { pageConstructor }]) => ({ route, pageConstructor }))
 
 		for (const { pageConstructor, route } of decoratorRoutes) {

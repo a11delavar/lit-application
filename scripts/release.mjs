f9ac33981a1b200534416d7cb823d4b2f5be4a41
arshia11d <arshia11d@outlook.com>
2023-03-17 21:46:06 +0100

    chore: Update dependencies

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -1,14 +1,7 @@
 // @ts-check
-import { Arguments, Packages, run } from './util/index.mjs'
+import { Arguments, Packages } from './util/index.mjs'
 
 const packageName = Arguments.tryGet(0, 'No package provided')
 const versionBumpType = Arguments.tryGet(1, 'No version bump type provided')
 
-const packageDirectory = Packages.getDirectory(packageName)
-
-// await run('npm run clean')
-await run('npm install', Packages.getDirectory('@a11d/lit-application'))
-await run('tsc', packageDirectory)
-await run(`npm version ${versionBumpType}`, packageDirectory)
-await run('npm publish --access public', packageDirectory)
-await run('npm run clean')
+await Packages.release(packageName, versionBumpType)

e4cda9509fd3400a7b8cf583758170e4ed7f646a
a11delavar <a11delavar@outlook.com>
2023-08-10 13:49:44 +0200

    feat(ApiJwt): Add ability to provide JWT refresh tokens

--- packages/ApiJwt/JwtApiAuthenticator.ts
+++ packages/ApiJwt/JwtApiAuthenticator.ts
@@ -1,8 +1,13 @@
-import { apiAuthenticator, ApiAuthenticator } from '@a11d/api'
+import { apiAuthenticator, ApiAuthenticator, FetchAction } from '@a11d/api'
+
+type JwtApiAuthenticatorOptions = {
+	readonly refresh?: (refreshToken: string) => Promise<string>
+}
 
 @apiAuthenticator()
 export class JwtApiAuthenticator implements ApiAuthenticator {
 	private static readonly tokenStorageKey = 'JwtApiAuthenticator.Token'
+	private static readonly refreshTokenStorageKey = 'JwtApiAuthenticator.RefreshToken'
 
 	static get token() { return localStorage.getItem(JwtApiAuthenticator.tokenStorageKey) ?? undefined }
 	static set token(value) {
@@ -13,16 +18,30 @@ export class JwtApiAuthenticator implements ApiAuthenticator {
 		}
 	}
 
-	authenticate(data: string) {
-		JwtApiAuthenticator.token = data
+	static get refreshToken() { return localStorage.getItem(JwtApiAuthenticator.refreshTokenStorageKey) ?? undefined }
+	static set refreshToken(value) {
+		if (value) {
+			localStorage.setItem(JwtApiAuthenticator.refreshTokenStorageKey, value)
+		} else {
+			localStorage.removeItem(JwtApiAuthenticator.refreshTokenStorageKey)
+		}
+	}
+
+	constructor(readonly options?: JwtApiAuthenticatorOptions) { }
+
+	authenticate(token: string, refreshToken?: string) {
+		JwtApiAuthenticator.token = token
+		JwtApiAuthenticator.refreshToken = refreshToken
 	}
 
 	unauthenticate() {
 		JwtApiAuthenticator.token = undefined
+		JwtApiAuthenticator.refreshToken = undefined
 	}
 
 	isAuthenticated() {
 		return !!JwtApiAuthenticator.token
+			&& (!this.options?.refresh || !!JwtApiAuthenticator.refreshToken)
 	}
 
 	processRequest(request: RequestInit): RequestInit {
@@ -32,5 +51,17 @@ export class JwtApiAuthenticator implements ApiAuthenticator {
 			headers.set('Authorization', `Bearer ${token}`)
 		}
 		return request
+	}
+
+	// TODO: Maximum tries: 3
+	async processResponse(response: Response, fetchAction: FetchAction) {
+		if (this.options?.refresh && response.status === 401 && JwtApiAuthenticator.refreshToken) {
+			const refreshedToken = await this.options.refresh(JwtApiAuthenticator.refreshToken)
+			if (refreshedToken) {
+				JwtApiAuthenticator.token = refreshedToken
+				return fetchAction()
+			}
+		}
+		return response
 	}
 }

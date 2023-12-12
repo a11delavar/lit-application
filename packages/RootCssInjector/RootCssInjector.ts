0cdf29fdd294bcfc9f6a1fe61719d749ce9b3329
arshia11d <arshia11d@outlook.com>
2023-12-12 05:42:54 +0100

    chore(RootCssInjector): SSR compatibility

--- packages/RootCssInjector/RootCssInjector.ts
+++ packages/RootCssInjector/RootCssInjector.ts
@@ -1,7 +1,12 @@
-import { CSSResult } from '@a11d/lit'
+import { CSSResult, isServer } from '@a11d/lit'
 
 export class RootCssInjector {
-	static inject(styles: CSSResult, styleElement = document.createElement('style')) {
+	static inject(styles: CSSResult, styleElement?: HTMLStyleElement) {
+		if (isServer) {
+			return
+		}
+
+		styleElement ??= document.createElement('style')
 		styleElement.innerHTML = styles.cssText
 		document.head.appendChild(styleElement)
 		return styleElement
